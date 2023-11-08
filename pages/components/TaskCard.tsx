dbd709c7c17c8796fe182fe8bd9f2a1ce07a78fc
Abhishek0712 <mabhishek712@gmail.com>
2023-11-08 14:49:11 +0530

    first commit

---
+++ pages/components/TaskCard.tsx
@@ -0,0 +1,145 @@
+import { useState } from "react";
+import TrashIcon from "../icons/TrashIcon";
+import { Id, Task } from "../types";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import ClockIcon from "../icons/ClockIcon";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+interface Props {
+  task: Task;
+  deleteTask: (id: Id) => void;
+  updateTask: (id: Id, content: string) => void;
+}
+
+function TaskCard({ task, deleteTask, updateTask }: Props) {
+  const [mouseIsOver, setMouseIsOver] = useState(false);
+  const [editMode, setEditMode] = useState(true);
+  const [completionDate, setCompletionDate] = useState<Date | null>(null);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  // console.log("setCompletionDate", setCompletionDate);
+
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: task.id,
+    data: {
+      type: "Task",
+      task,
+    },
+    disabled: editMode,
+  });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
+  const toggleEditMode = () => {
+    setShowDatePicker(true);
+    setEditMode((prev) => !prev);
+    setMouseIsOver(false);
+  };
+
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="
+        opacity-30
+      bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl border-2 border-rose-500  cursor-grab relative
+      "
+      />
+    );
+  }
+
+  if (editMode) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        {...attributes}
+        {...listeners}
+        className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative"
+      >
+        <textarea
+          className="
+        h-[90%]
+        w-full resize-none border-none rounded bg-transparent text-white focus:outline-none
+        "
+          value={task.content}
+          autoFocus
+          placeholder="Task content here"
+          onBlur={toggleEditMode}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && e.shiftKey) {
+              toggleEditMode();
+            }
+          }}
+          onChange={(e) => updateTask(task.id, e.target.value)}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      onClick={toggleEditMode}
+      className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative task"
+      onMouseEnter={() => {
+        setMouseIsOver(true);
+      }}
+      onMouseLeave={() => {
+       setMouseIsOver(false);
+      }}
+    >
+      <p className="my-auto h-[90%] w-full overflow-y-auto overflow-x-hidden whitespace-pre-wrap">
+        {task.content}
+        </p>
+       
+            
+            {mouseIsOver && (
+        <button
+          onClick={() => {
+            deleteTask(task.id);
+          }}
+          className="stroke-white absolute right-4 top-1/2 -translate-y-1/2 bg-columnBackgroundColor p-2 rounded opacity-60 hover:opacity-100"
+        >
+          <TrashIcon />
+        </button>
+      )}
+        
+      <div className="flex justify-between items-start absolute left-4 bottom-1">
+    <p><ClockIcon  />{task.completionDate}</p>
+    <div onClick={(event) => event.stopPropagation()}>
+  {showDatePicker && (
+    <DatePicker
+      selected={completionDate}
+      onChange={(date: Date) => {
+        setCompletionDate(date);
+        setShowDatePicker(false);
+      }}
+    />
+  )}
+  {!showDatePicker && completionDate && (
+    <p>Due Date: {completionDate.toLocaleDateString()}</p>
+  )}
+</div>
+</div>
+      
+    </div>
+  );
+}
+
+export default TaskCard;
